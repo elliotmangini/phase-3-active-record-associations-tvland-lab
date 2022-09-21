ad9e20dfe994151136f98607e54d5924c8b9559f
Elliot Mangini <elliot.mangini@gmail.com>
2022-09-21 12:38:01 -0600

    that was tough but i did it

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,32 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        output = []
+        characters.map do |character|
+            showid = character.show_id
+            output << "#{character.name} - #{shows.find(showid).name}"
+        end
+        output
+    end
+
+
+    # solution version:
+    # def list_roles
+    #     self.characters.map do |character|
+    #       "#{character.name} - #{character.show.name}"
+    #     end
+    # end
+
+    #because map already returns an array
+
+
+
 end
